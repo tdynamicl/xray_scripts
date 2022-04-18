43384726e142547339584b0f949d66212f4e58b8
tdynamicl <405019553@qq.com>
2022-04-18 15:20:18 +0800

    函数重复

--- cert_renew.sh
+++ cert_renew.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+### 参数配置
 PWD=$(cd `dirname $0`; pwd)
 CONF_FILE_PATH="./config.ini"
 # 配置中的参数
 DOMAIN=$(awk -F '=' '/\[main\]/{a=1}a==1&&$1~/domain/{print $2;exit}' ${CONF_FILE_PATH})
 
+### 函数
 # 打印日志函数
 function echo_log () {
   echo `date +"[%Y-%m-%d %H:%M:%S] "`"$1" >> "${PWD}/${0}.log"
 }
 
+### 开始
 echo_log 'Ssl cert renew start'
 
 # test param
@@ -30,10 +33,3 @@ systemctl start xray
 echo 'nginx xray started' >> "${PWD}/${0}.log"
 
 echo_log 'Renew complete'
-
-# function readconfig
-
-# function echo log
-function echo_log () {
-  echo `date +"[%Y-%m-%d %H:%M:%S] "`"$1" >> "${PWD}/${0}.log"
-}
